534255d01c7ba7238961cc37f4aed14007d2d702
85bogdan <daniel.t.bogucki@gmail.com>
2017-07-07 17:45:13 +0200

    add js-hint

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,41 +13,44 @@ module.exports = function(grunt) {
   		}
   	},
 
-  	imagemin: {
-  		dynamic: {
-  			files: [{
-  				expand: true,
-  				cwd: 'images/',
-  				src: ['**/*.{png,jpg,gif}'],
-  				dest: 'images/build/'
-  			}]
-  		}
-  	},
-        watch: {
-    		scripts: {
-        		files: ['sass/*.sass'],
-        		tasks: ['sass'],
-        		options: {
-            			spawn: false,
-        		},
-    		}
-	},
-        browserSync: {
-            dev: {
-                bsFiles: {
-                    src : [
-                        'css/*.css',
-                        '*.html',
-                   ]
-                },
-                options: {
-                        server: {
-			baseDir: "./"
+    imagemin: {
+		  dynamic: {
+  		  files: [{
+  			 expand: true,
+  			 cwd: 'images/',
+  			 src: ['**/*.{png,jpg,gif}'],
+  			 dest: 'images/build/'
+  		  }]
+      }
+    },
+    
+    watch: {
+    	scripts: {
+        files: ['sass/*.sass'],
+        tasks: ['sass'],
+        options: {
+          spawn: false,
+        },
+    	}
+    },
 
-                }
-            }
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src : ['css/*.css',
+                  '*.html',
+                ]
+        },
+        options: {
+          server: {
+			     baseDir: "./"
+          }
         }
+      }
+    }, 
 
+    jshint: {
+      all: ['js/*.js']
     }
   });
 
@@ -56,7 +59,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-browser-sync');
+  grunt.loadNpmTasks('grunt-contrib-jshint')
   // Default task(s).
 
-  grunt.registerTask('default', ['sass', 'imagemin', 'watch']);
+  grunt.registerTask('default', ['jshint']);
 };
