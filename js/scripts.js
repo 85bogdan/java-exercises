1a803f11d0109530c940f2a67ce86bb1aaa924c5
85bogdan <daniel.t.bogucki@gmail.com>
2017-07-10 20:38:50 +0200

    correction

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ function getTriangleArea(a,h) {
 		return (a * h  / 2)
 	}
 };
-console.log(getTriangleArea(10,0));
+console.log(getTriangleArea(10,6));
 var triangle1Area = getTriangleArea(10,15);
 var triangle2Area = getTriangleArea(7,25);
 var triangle3Area = getTriangleArea(3,8);
